36a0ce0b64f594ed2fdbefdecbcb78f557b121b6
wjb127 <30383528+wjb127@users.noreply.github.com>
2020-08-29 20:48:59 +0900

    Create 9657 돌 게임 3.cpp

---
+++ 동적계획법(DP)/9657 돌 게임 3.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    
+    if(n%7==2 || n%7==0)
+        cout<<"CY"<<endl;
+    else
+        cout<<"SK"<<endl;
+    
+    
+    return 0;
+}
