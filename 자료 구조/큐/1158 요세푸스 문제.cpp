0065af6da888b26f5cc9dc115f1f71f6ace15bb0
wjb127 <30383528+wjb127@users.noreply.github.com>
2020-08-29 21:50:18 +0900

    Create 1158 요세푸스 문제.cpp

---
+++ 자료 구조/큐/1158 요세푸스 문제.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<memory.h>
+#include<algorithm>
+#include<list>
+using namespace std;
+
+int n,m;
+
+int arr[5001];
+
+int main()
+{
+    cin>>n>>m;
+    
+    list<int> seq;
+    
+    for(int i=1;i<=n;i++)
+    {
+        seq.push_back(i);
+    }
+    
+    list<int>::iterator kill = seq.begin();
+    
+    for(int i=0;i<m-1;i++)
+        kill++;
+    
+    for(int i=0;i<n;i++)
+    {
+        arr[i] = *kill;
+        kill = seq.erase(kill);
+        if(kill == seq.end())
+            kill = seq.begin();
+        
+        for(int j=0;j<m-1;j++)
+        {
+            kill++;
+            if(kill == seq.end())
+                kill = seq.begin();
+        }
+        
+        
+    }
+    
+    
+    cout<<"<";
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i];
+        if(i!=n-1)
+            cout<<", ";
+    }
+    cout<<">"<<endl;
+    
+    return 0;
+}
